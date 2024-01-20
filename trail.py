e4f5e000c2c35f658ba722d5dc5ea00e1024a93f
Champion_Samay <samaypandey2022@kccemsr.edu.in>
2024-01-20 19:18:39 +0530

    checking reference

--- trail.py
+++ trail.py
@@ -4,5 +4,7 @@ from firebase_admin import db,credentials
 cred=credentials.Certificate("credentials.json")
 firebase_admin.initialize_app(cred,{'databaseURL':'https://hr-management-system-f7c9f-default-rtdb.asia-southeast1.firebasedatabase.app/'})
 
-ref = db.reference('/')
-ref.update({'name':'Samay'})
+admins = db.reference('admins')
+to_search='Samay'
+password1 = admins.child(to_search).child('password').get()
+print(password1)
