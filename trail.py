1993e0b1d3c7073daf479e203dd757224bd02143
Champion_Samay <samaypandey2022@kccemsr.edu.in>
2024-01-23 19:42:22 +0530

    trial file setup

--- trail.py
+++ trail.py
@@ -4,7 +4,7 @@ from firebase_admin import db,credentials
 cred=credentials.Certificate("credentials.json")
 firebase_admin.initialize_app(cred,{'databaseURL':'https://hr-mgntsystem-test.asia-southeast1.firebasedatabase.app/'})
 
-admins = db.reference('admins')
+admins = db.reference('Trial')
 to_search='Samay'
 password1 = admins.child(to_search).child('password').get()
 print(password1)
