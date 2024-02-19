d8f815bd1e3ae4abccad7aee4436348befd54b65
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2024-02-19 17:28:32 +0530

    updated credits dialog box

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ class CreativeLoginApp:
         self.company_name_text = self.company_logo_canvas.create_text(
             window_width / 2,
             100,
-            text="Ben Dover Inc",
+            text="Hobby Lobby Inc",
             font=("Helvetica", 28, "bold"),
             fill="white",
         )
@@ -169,7 +169,7 @@ class CreativeLoginApp:
         # You can customize the credits information as per your needs
         credits_text = (
             "HR Management System\n\n"
-            "Developed by: \n -Samay Pandey, \n-Armaan Nakhuda, \n-Sushant Navle, \n-Peeyush Karnik(assuming he does any work)\n\n"
+            "Developed by: \n -Samay Pandey, \n-Armaan Nakhuda, \n-Sushant Navle, \n-Peeyush Karnik\n\n"
             "Stage Completition: Stage 4 completed\n Stage 5/6 in progress\n"
             "Date: 6th Feb 2024\n"
             "\nSpecial Thanks to:\n- Firebase\n- OpenAI\n- Yash Patil\n- Zane Fernandes\n- Ninad Walke\n"
